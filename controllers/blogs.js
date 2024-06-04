bd7fab1a4d86f2380cfda427a346fef0d415c8a1
Your Name <you@example.com>
2024-06-04 22:37:22 +0200

    first commit

---
+++ controllers/blogs.js
@@ -0,0 +1,24 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.post("/", (request, response, next) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .then((savedBlog) => {
+      response.json(savedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
